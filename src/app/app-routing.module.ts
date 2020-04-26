7ee49c658e6d76bcb7a72d22f90fa490e91f4c88
Sony Lao <sony@Sonys-iMac.local>
2020-04-26 01:33:35 -0500

    routing working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameViewComponent } from './views/game-view/game-view.component';
+import { GameContainerComponent } from './views/game-container/game-container.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: GameContainerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
